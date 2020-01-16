cdaee682a13ae6dd3f44cf785ef3b4cd750ecd57
wesleymutwiri <wesleymochiemo@gmail.com>
2020-01-17 00:43:22 +0300

    created and finished a svelte meetups app

---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,78 @@
+<script>
+  import {
+    createEventDispatcher
+  } from "svelte";
+  import {
+    scale
+  } from 'svelte/transition';
+  import {
+    flip
+  } from 'svelte/animate';
+
+  import MeetupItem from "./MeetupItem.svelte";
+  import MeetupFilter from "./MeetupFilter.svelte";
+  import Button from "../UI/Button.svelte";
+
+  export let meetups;
+
+  const dispatch = createEventDispatcher();
+
+  let favsOnly = false;
+
+  $: filteredMeetups = favsOnly ? meetups.filter(m => m.isFavorite) : meetups;
+
+  function setFilter(event) {
+    favsOnly = event.detail === 1;
+  }
+</script>
+
+<style>
+  #meetups {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  #no-meetups {
+    margin: 1rem;
+  }
+
+  #meetup-controls {
+    margin: 1rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  @media (min-width: 768px) {
+    #meetups {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
+
+<section id="meetup-controls">
+  <MeetupFilter on:select={setFilter} />
+  <Button on:click={()=> dispatch('add')}>New Meetup</Button>
+</section>
+{#if filteredMeetups.length === 0}
+<p id="no-meetups">No meetups found</p>
+{/if}
+<section id="meetups">
+  {#each filteredMeetups as meetup (meetup.id)}
+  
+  <div transition:scale animate:flip>
+  <MeetupItem
+    id={meetup.id}
+    title={meetup.title}
+    subtitle={meetup.subtitle}
+    description={meetup.description}
+      imageUrl={meetup.imageUrl}
+      email={meetup.contactEmail}
+      address={meetup.address}
+      isFav={meetup.isFavorite}
+      on:showdetails
+      on:edit />
+    </div>
+      {/each}
+</section>
---
+++ src/UI/Error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Modal from './Modal.svelte';
+    export let message;
+
+    function clearError() {
+        error = null;
+    }
+</script>
+
+<Modal title="An error occured" on:cancel={clearError}>
+    <p>{message}</p>
+</Modal>
